a9d8dc9d022f9ec19a9a4c80c7c6b32e4711ca79
solanobayona <est.cristian.solano@unimilitar.edu.co>
2026-04-24 12:58:46 -0500

    nueva funcionalidad: implementación bresenham y trazado de líneas

--- script.js
+++ script.js
@@ -45,3 +45,27 @@ function midpointCircle(centerX, centerY, r, color) {
         drawOctants(centerX, centerY, x, y);
     }
 }
+function bresenhamLine(x0, y0, x1, y1, color) {
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+    let sx = (x0 < x1) ? 1 : -1;
+    let sy = (y0 < y1) ? 1 : -1;
+    let err = dx - dy; // Parámetro de decisión err para controlar el error acumulado
+
+    while (true) {
+        plotPixel(ctx, x0, y0, color);
+        if (x0 === x1 && y0 === y1) break;
+        
+        let e2 = 2 * err;
+        if (e2 > -dy) {
+            // Actualización del parámetro de decisión: nos movemos en X
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dx) {
+            // Actualización del parámetro de decisión: nos movemos en Y
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
